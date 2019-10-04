9e13744d58d572948b71d4f92c376e8371095cff
Erika Eckfeld <erikazprincess@aol.com>
2019-10-03 20:22:51 -0500

    Built functionality for challenge

---
+++ Colors/Colors/ColorsViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ColorsViewController.swift
+//  Colors
+//
+//  Created by Erika Eckfeld on 10/3/19.
+//  Copyright © 2019 Erika Eckfeld. All rights reserved.
+//
+
+import UIKit
+
+class ColorsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var colors = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        cell.textLabel?.text = colors[indexPath.row]
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
